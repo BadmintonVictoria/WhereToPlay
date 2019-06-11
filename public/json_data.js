b717ac9e254fdae2e59b6afe4204b008c41d6d19
BadmintonVictoria <travel_kev@yahoo.com>
2019-06-10 21:58:00 -0700

    Create Vue JS dynamic site version

---
+++ public/json_data.js
@@ -0,0 +1,6 @@
+var json_update = '2019-06-10';
+
+var json_data = [
+{"DayOfWeek":"Monday", "GroupDescription":"Drop In Ladies Doubles", "GeneralArea":"Cordova Bay", "Location":"Brentwood Bay Hall", "PlayerLevel":"Intermediate", "PlayerAge":"Adult", "Shuttle":"Feather", "TimeRange":"8:00 pm - 10:00 pm", "Season":"Unknown"},
+{"DayOfWeek":"Monday", "GroupDescription":"Drop-in Competitive Ladder", "GeneralArea":"Cordova Bay", "Location":"Brentwood Bay Hall", "PlayerLevel":"Advanced", "PlayerAge":"Adult", "Shuttle":"Feather", "TimeRange":"8:00 pm - 10:30 pm", "Season":"Unknown"}
+];
